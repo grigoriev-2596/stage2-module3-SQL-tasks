a0f7e1587484647692a18ca6bf71d5d1d0c5a17f
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-11-22 16:36:36 +0300

    fixed delete student and select with name or marks

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE groupnumber > 4;
+DELETE FROM student WHERE id IN (SELECT DISTINCT s.id FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id INNER JOIN subject AS sub ON m.subject_id = sub.id WHERE sub.grade >= 4);
 DELETE FROM student WHERE id IN (SELECT DISTINCT s.id FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id WHERE m.mark < 4);
 DELETE FROM paymenttype WHERE id = 1;
 DELETE FROM mark WHERE mark < 7;
